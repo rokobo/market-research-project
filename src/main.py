d01a9bfd13bfa0cc99f3d39af790900a9e11e4a4
Kobos <61996066+rokobo@users.noreply.github.com>
2024-07-11 08:38:14 -0300

    duplicate row id bug fix

--- src/main.py
+++ src/main.py
@@ -39,9 +39,9 @@ app.layout = [
 
 if __name__ == "__main__":
     app.run_server(
-        host="0.0.0.0",
+        # host="0.0.0.0",
         port="8060",
-        # debug=True,
-        # dev_tools_hot_reload=True,
-        # use_reloader=True
+        debug=True,
+        dev_tools_hot_reload=True,
+        use_reloader=True
     )
