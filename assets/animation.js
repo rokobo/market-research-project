9dd225c775ec45ab080f6990cd02ddc9c2f2f0d0
Kobos <61996066+rokobo@users.noreply.github.com>
2024-07-15 07:40:41 -0300

    assets

--- assets/animation.js
+++ assets/animation.js
@@ -1,133 +1,142 @@
-// // Inspired bv https://codepen.io/uiswarup/pen/YzJEPNe
-// const gravity = 0.5;
-// const terminalVelocity = 5;
-// const drag = 0.075;
-// const colors = [
-//     { front: '#FF0000', back: '#8B0000' },
-//     { front: '#00cc00', back: '#006400' },
-//     { front: '#0000FF', back: '#00008B' },
-//     { front: '#FFFF00', back: '#b3b300' },
-//     { front: '#FFA500', back: '#805300' },
-//     { front: '#FFC0CB', back: '#800015' },
-//     { front: '#990099', back: '#660066' },
-//     { front: '#40E0D0', back: '#126e64' }];
-// const confettiQuantityRatio = 5000;
+// Inspired bv https://codepen.io/uiswarup/pen/YzJEPNe
+const gravity = 0.5;
+const terminalVelocity = 5;
+const drag = 0.075;
+const colors = [
+    { front: '#FF0000', back: '#8B0000' },
+    { front: '#00cc00', back: '#006400' },
+    { front: '#0000FF', back: '#00008B' },
+    { front: '#FFFF00', back: '#b3b300' },
+    { front: '#FFA500', back: '#805300' },
+    { front: '#FFC0CB', back: '#800015' },
+    { front: '#990099', back: '#660066' },
+    { front: '#40E0D0', back: '#126e64' }];
+const confettiQuantityRatio = 5000;
 
-// randomRange = (min, max) => Math.random() * (max - min) + min;
+randomRange = (min, max) => Math.random() * (max - min) + min;
 
-// function animateConfetti() {
-//     let confetti = [];
-//     var retryInterval = setInterval(function() {
-//         console.log("Began confetti animation");
-//         const canvas = document.getElementById("confetti");
+function animateConfetti() {
+    let confetti = [];
+    var retryInterval = setInterval(function() {
+        console.log("Began confetti animation");
+        const canvas = document.getElementById("confetti");
 
-//         if (canvas) {
-//             clearInterval(retryInterval);
-//             const ctx = canvas.getContext("2d");
+        if (canvas) {
+            clearInterval(retryInterval);
+            const ctx = canvas.getContext("2d");
 
-//             function setCanvasSize() {
-//                 canvas.width = window.innerWidth;
-//                 canvas.height = Math.max(document.body.scrollHeight, window.innerHeight);
-//                 confettiCount = (canvas.width * canvas.height / confettiQuantityRatio) | 0;
-//                 console.log("Canvas set to " + canvas.width + "x" + canvas.height);
-//                 console.log("Using " + confettiCount + " units of confetti");
-//             }
+            function setCanvasSize() {
+                canvas.width = window.innerWidth;
+                canvas.height = Math.max(document.body.scrollHeight, window.innerHeight);
+                confettiCount = (canvas.width * canvas.height / confettiQuantityRatio) | 0;
+                console.log("Canvas set to " + canvas.width + "x" + canvas.height);
+                console.log("Using " + confettiCount + " units of confetti");
+            }
 
-//             setCanvasSize();
+            setCanvasSize();
 
-//             class Confetti {
-//                 constructor() {
-//                     this.x = Math.random() * canvas.width;
-//                     this.y = Math.random() * canvas.height;
-//                     this.color = colors[Math.floor(Math.random() * colors.length)];
-//                     this.size_x = (Math.random() * 10) + 10;
-//                     this.size_y = (Math.random() * 20) + 10;
-//                     this.rotation = Math.random() * 2 * Math.PI;
-//                     this.rotation_speed_y = ((Math.random() * 5) / 100) + 0.05;
-//                     this.rotation_speed_z = Math.random() / 10;
-//                     this.scale_x = 1;
-//                     this.scale_y = 1;
-//                     this.velocity_x = (Math.random() * 50) - 25;
-//                     this.velocity_y = Math.random() * -50;
-//                     this.width = this.size_x * this.scale_x;
-//                     this.height =  this.size_y * this.scale_y;
-//                 }
-//                 update() {
-//                     // Move canvas to position and rotate
-//                     ctx.translate(this.x, this.y);
-//                     ctx.rotate(this.rotation);
+            class Confetti {
+                constructor() {
+                    this.x = Math.random() * canvas.width;
+                    this.y = Math.random() * canvas.height;
+                    this.color = colors[Math.floor(Math.random() * colors.length)];
+                    this.size_x = (Math.random() * 10) + 10;
+                    this.size_y = (Math.random() * 20) + 10;
+                    this.rotation = Math.random() * 2 * Math.PI;
+                    this.rotation_speed_y = ((Math.random() * 5) / 100) + 0.05;
+                    this.rotation_speed_z = Math.random() / 10;
+                    this.scale_x = 1;
+                    this.scale_y = 1;
+                    this.velocity_x = (Math.random() * 50) - 25;
+                    this.velocity_y = Math.random() * -50;
+                    this.width = this.size_x * this.scale_x;
+                    this.height =  this.size_y * this.scale_y;
+                }
+                update() {
+                    // Move canvas to position and rotate
+                    ctx.translate(this.x, this.y);
+                    ctx.rotate(this.rotation);
 
-//                     // Apply forces to velocity
-//                     this.velocity_x -= this.velocity_x * drag;
-//                     this.velocity_y = Math.min(this.velocity_y + gravity, terminalVelocity);
-//                     this.velocity_x += Math.random() > 0.5 ? Math.random() : -Math.random();
+                    // Apply forces to velocity
+                    this.velocity_x -= this.velocity_x * drag;
+                    this.velocity_y = Math.min(this.velocity_y + gravity, terminalVelocity);
+                    this.velocity_x += Math.random() > 0.5 ? Math.random() : -Math.random();
 
-//                     // Set position
-//                     this.x += this.velocity_x;
-//                     this.y += this.velocity_y;
+                    // Set position
+                    this.x += this.velocity_x;
+                    this.y += this.velocity_y;
 
-//                     // Apply rotation along the z axis
-//                     this.rotation = this.rotation + this.rotation_speed_z;
-//                     if (this.rotation > Math.PI * 2) {
-//                         this.rotation = 0;
-//                     }
-//                 }
-//                 draw() {
-//                     this.width = this.size_x * this.scale_x;
-//                     this.height =  this.size_y * this.scale_y;
+                    // Apply rotation along the z axis
+                    this.rotation = this.rotation + this.rotation_speed_z;
+                    if (this.rotation > Math.PI * 2) {
+                        this.rotation = 0;
+                    }
+                }
+                draw() {
+                    this.width = this.size_x * this.scale_x;
+                    this.height =  this.size_y * this.scale_y;
 
-//                     // Loop confetto x position
-//                     if (this.x > canvas.width) {this.x = 0}
-//                     if (this.x < 0) {this.x = canvas.width}
+                    // Loop confetto x position
+                    if (this.x > canvas.width) {this.x = 0}
+                    if (this.x < 0) {this.x = canvas.width}
 
-//                     // Spin confetto by scaling y, draw, reset transform matrix
-//                     this.scale_y = Math.cos(this.y * this.rotation_speed_y);
-//                     ctx.fillStyle = this.scale_y > 0 ? this.color.front : this.color.back;
-//                     ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
-//                     ctx.setTransform(1, 0, 0, 1, 0, 0);
-//                 }
-//             }
+                    // Spin confetto by scaling y, draw, reset transform matrix
+                    this.scale_y = Math.cos(this.y * this.rotation_speed_y);
+                    ctx.fillStyle = this.scale_y > 0 ? this.color.front : this.color.back;
+                    ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+                    ctx.setTransform(1, 0, 0, 1, 0, 0);
+                }
+            }
 
-//             function createConfetti() {
-//                 for (let i = 0; i < confettiCount; i++) {
-//                     confetti.push(new Confetti());
-//                 }
-//             }
+            function createConfetti() {
+                for (let i = 0; i < confettiCount; i++) {
+                    confetti.push(new Confetti());
+                }
+            }
 
-//             function animate() {
-//                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-//                 confetti.forEach((confetto, index) => {
-//                     confetto.update();
+            function animate() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                confetti.forEach((confetto, index) => {
+                    confetto.update();
 
-//                     // Delete confetti when out of frame, else draw
-//                     if (confetto.y >= canvas.height) {
-//                         confetti.splice(index, 1);
-//                     } else {
-//                         confetto.draw();
-//                     }
-//                 });
-//                 if (confetti.length > 0) {
-//                     requestAnimationFrame(animate);
-//                 } else {
-//                     console.log("Finished confetti animation")
-//                 }
-//             }
+                    // Delete confetti when out of frame, else draw
+                    if (confetto.y >= canvas.height) {
+                        confetti.splice(index, 1);
+                    } else {
+                        confetto.draw();
+                    }
+                });
+                if (confetti.length > 0) {
+                    requestAnimationFrame(animate);
+                } else {
+                    console.log("Finished confetti animation")
+                }
+            }
 
-//             createConfetti();
-//             animate();
-//         }
-//     }, 500)
-// };
+            createConfetti();
+            animate();
+        }
+    }, 500)
+};
 
-// var retryInterval = setInterval(function() {
-//     const save_button = document.getElementById("confirm-send");
-
-//     if (save_button) {
-//         console.log("Button element found");
-//         clearInterval(retryInterval);
-//         save_button.addEventListener("click", function() {
-//             console.log("Button clicked");
-//             animateConfetti();
-//         })
-//     }
-// });
+var retryInterval = setInterval(function() {
+    const save_button = document.getElementById("save-products");
+    if (save_button) {
+        console.log("First button element found");
+        clearInterval(retryInterval);
+        save_button.addEventListener("click", function() {
+            console.log("First button clicked");
+            var retryInterval2 = setInterval(function() {
+                const save_button = document.getElementById("close-send-confirmation");
+                if (save_button) {
+                    console.log("Second button element found");
+                    clearInterval(retryInterval2);
+                    save_button.addEventListener("click", function() {
+                        console.log("Second button clicked");
+                        animateConfetti();
+                    }, 1000)
+                }
+            });
+        }, 1000)
+    }
+});
--- assets/callbacks.js
+++ assets/callbacks.js
@@ -1,136 +1,123 @@
 if (!window.dash_clientside) {window.dash_clientside = {}}
 const PRODUCTS = ["acucar","arroz","cafe","farinha","feijao","leite","manteiga","soja","banana","batata","tomate","carne","pao"];
 const FIELDS = ["brand", "price", "quantity", "obs"];
-
 const groupValidations2=(lists,keys)=>lists.reduce((acc,_,i)=>{if(i%4===0&&keys[i/4]){acc[keys[i/4]]=lists.slice(i,i+4).flat()}return acc},{});
 
 window.dash_clientside.clientside = {
-    clear_contents: function(click) {
-        if (typeof click !== 'number') {return window.dash_clientside.no_update};
-        console.log("CALL clear contents");
-        return [];
-    },
-    save_state: function(_1, _2, load_flag, name, date, establishment, observations, ...products) {
-        if (load_flag === null) {return window.dash_clientside.no_update}
-
-        let data = [];
-        data.push({'first': [name, date, establishment]});
-        data.push({'observations': observations});
-
-        for (let i = 0; i < products.length; i++) {
-            let product_name = PRODUCTS[i];
-            let product = products[i];
-
-            for (let row of product) {
-                let row_id = row.props.id;
-                let val = row.props.children;
-                let vals = {};
-
-                for (let idx = 0; idx < val.length; idx++) {
-                    try {vals[idx] = val[idx].props.children.props.value} catch (error) {}
-                }
-
-                let values;
-                switch (val.length) {
-                    case 4:
-                        values = [vals[1], vals[2], vals[3], null];
-                        break;
-                    case 3:
-                        if (typeof vals[1] === 'string') {values = [vals[1], vals[2], null, null]}
-                        else {values = [null, vals[1], null, vals[2]]}
-                        break;
-                    default:
-                        values = [null, null, null, null];
-                }
-                data.push({"container": product_name, "values": values, "row_id": row_id.split("-").slice(-1)[0]});
+clear_contents: function(click) {
+    if (typeof click !== 'number') {return window.dash_clientside.no_update};
+    console.log("CALL clear contents");
+    return [];
+},
+close_modal: function(click) {
+    if (typeof click !== 'number') {return window.dash_clientside.no_update};
+    return false;
+},
+save_state: function(_1, _2, load_flag, name, date, establishment, observations, ...products) {
+    if (load_flag === null) {return window.dash_clientside.no_update}
+    let data = [];
+    data.push({'first': [name, date, establishment]});
+    data.push({'observations': observations});
+    for (let i = 0; i < products.length; i++) {
+        let product_name = PRODUCTS[i];
+        let product = products[i];
+        for (let prod_row of product) {
+            let row_id = prod_row.props.id;
+            let val = prod_row.props.children;
+            let vals = {};
+            for (let idx = 0; idx < val.length; idx++) {
+                try {vals[idx] = val[idx].props.children.props.value} catch (error) {}}
+            let row;
+            switch (val.length) {
+                case 4:
+                    row = [vals[1], vals[2], vals[3], null];
+                    break;
+                case 3:
+                    if (typeof vals[1] === 'string') {vals = [vals[1], vals[2], null, null]}
+                    else {row = [null, vals[1], null, vals[2]]}
+                    break;
+                default:
+                    row = [null, null, null, null];
             }
+            data.push({"container": product_name, "values": row, "row_id": row_id.split("-").slice(-1)[0]});
         }
-        console.log("CALL save data", data);
-        return data;
-    },
-    validate_args: function (_1, name, date, est, ...values) {
-        var first_args = values.splice(-3);
-        var slice_length = values.length - PRODUCTS.length;
-        var second_args = values.slice(slice_length/2 + PRODUCTS.length);
-        var validations = [];
-        var status1 = [];
-        var status2 = [];
-
-        // Individual validations
-        for (var i = 0; i < second_args.length; i += 4) {
-            var [br, pr, qn, obs] = second_args.slice(i, i + 4);
-            validations.push(br.map(brand => typeof brand == 'string'));
-            validations.push(pr.map(price => typeof price === 'number' && !isNaN(price)));
-            validations.push(qn.map(() => true));
-            validations.push(obs.map(() => true));
-        };
-
-        // Section validations
-        for (var i = 0; i < validations.length; i += 4) {
-            const val = validations.slice(i, i + 4);
-            if (val.every(sublist => sublist.every(v => v))) {
-                if (val[1].length > 2) {
-                    status1.push("Completo");
-                    status2.push("success");
-                } else {
-                    status1.push("Okay");
-                    status2.push("warning");
-                }
-
+    };
+    console.log("CALL save data", data);
+    return data;
+},
+validate_args: function (_1, name, date, est, ...vals) {
+    var first_args = vals.splice(-3);
+    console.log(first_args);
+    var slice_length = vals.length - PRODUCTS.length;
+    var second_args = vals.slice(slice_length/2 + PRODUCTS.length);
+    var validations = [];
+    var status1 = [];
+    var status2 = [];
+    // Individual validations
+    for (var i = 0; i < second_args.length; i += 4) {
+        var [br, pr, qn, obs] = second_args.slice(i, i + 4);
+        validations.push(br.map(brand => typeof brand == 'string'));
+        validations.push(pr.map(price => typeof price === 'number' && !isNaN(price)));
+        validations.push(qn.map(() => true));
+        validations.push(obs.map(() => true));
+    };
+    // Section validations
+    for (var i = 0; i < validations.length; i += 4) {
+        const val = validations.slice(i, i + 4);
+        if (val.every(sublist => sublist.every(v => v))) {
+            if (val[1].length > 2) {
+                status1.push("Completo");
+                status2.push("success");
             } else {
-                status1.push("Faltando");
-                status2.push("danger");
+                status1.push("Okay");
+                status2.push("warning");
             }
-        };
-
-        // Transform to appropriate classnames
-        validations = validations.map(sublist => sublist.map(v => v ? "correct" : "wrong"));
-        console.log("CALL validation", groupValidations2(validations, PRODUCTS));
-        validations = validations.concat(status1).concat(status2).concat(
-            first_args.map(v => (v !== null && v !== "" ? "correct" : "wrong"))
-        );
-        validations.push("");
-        return validations;
-    },
-    delete_product_row: function (...values) {
-        var ctx = dash_clientside.callback_context.triggered_id;
-        if (values.slice(0, PRODUCTS.length).every(sublist => sublist.every(v => typeof v === "undefined"))) {
-            return window.dash_clientside.no_update;
+        } else {
+            status1.push("Faltando");
+            status2.push("danger");
         }
-
-        var children_states = values.slice(PRODUCTS.length);
-        var contextType = ctx.type.slice(7);
-        var prop_id = `${contextType}-product-row-${ctx.index}`
-        var index = PRODUCTS.indexOf(contextType);
-        var patched_children = Array(PRODUCTS.length).fill(window.dash_clientside.no_update);
-        var children = children_states[index];
-
-        for (let i = 0; i < children.length; i++) {
-            var child = children[i];
-            // Find row by row index and context index, remove child at index i
-            if (child.props.id === prop_id) {children.splice(i, 1);break;}
-        }
-        patched_children[index] = children;
-        console.log("CALL delete row:", PRODUCTS[index]);
-        return patched_children;
-    },
-    display_progress: function (_1, name, date, est, ...values) {
-        badges = values.slice(0, 13);
-        children = values.slice(13).map(c => c.length);
-        message = "Você tem certeza que quer enviar? Resumo do envio:\n\n";
-        message += "Seções com poucos itens: " + children.filter(v => v < 3).length;
-        return_vals = badges.map(v => {
-            if (v === "success") {return {"color": "green"}}
-            else if (v === "danger") {return {"color": "red"}}
-            else {return {"color": "#FCAE1E"}}
-        });
-
-        // Update save button status
-        if ([name, date, est].every(v => v == "correct") && badges.every(v => v != "danger")) {
-            return_vals = return_vals.concat([false, "success"])}
-        else {return_vals = return_vals.concat([true, "danger"])}
-        console.log("CALL progress:", [name, date, est], badges, return_vals);
-        return_vals.push(message);
-        return return_vals;
-    }
+    };
+    // Transform to appropriate classnames
+    validations = validations.map(sublist => sublist.map(v => v ? "correct" : "wrong"));
+    console.log("CALL validation", groupValidations2(validations, PRODUCTS));
+    validations = validations.concat(status1).concat(status2).concat(
+        first_args.map(v => (v !== null && v !== "" ? "correct" : "wrong")));
+    validations.push("");
+    return validations;
+},
+delete_product_row: function (...vals) {
+    var ctx = dash_clientside.callback_context.triggered_id;
+    if (vals.slice(0, PRODUCTS.length).every(sublist => sublist.every(v => typeof v === "undefined"))) {
+        return window.dash_clientside.no_update;}
+    var children_states = vals.slice(PRODUCTS.length);
+    var contextType = ctx.type.slice(7);
+    var prop_id = `${contextType}-product-row-${ctx.index}`
+    var index = PRODUCTS.indexOf(contextType);
+    var patched_children = Array(PRODUCTS.length).fill(window.dash_clientside.no_update);
+    var children = children_states[index];
+    for (let i = 0; i < children.length; i++) {
+        var child = children[i];
+        // Find row by row index and context index, remove child at index i
+        if (child.props.id === prop_id) {children.splice(i, 1);break;}}
+    patched_children[index] = children;
+    console.log("CALL delete row:", PRODUCTS[index]);
+    return patched_children;
+},
+display_progress: function (_1, name, date, est, ...vals) {
+    badges = vals.slice(0, 13);
+    children = vals.slice(13).map(c => c.length);
+    message = "Você tem certeza que quer enviar? Resumo do envio:\n\n";
+    message += "Seções com poucos itens: " + children.filter(v => v < 3).length;
+    return_vals = badges.map(v => {
+        if (v === "success") {return {"color": "green"}}
+        else if (v === "danger") {return {"color": "red"}}
+        else {return {"color": "#FCAE1E"}}});
+    // Update save button status
+    if ([name, date, est].every(v => v == "correct") && badges.every(v => v != "danger")) {
+        return_vals = return_vals.concat([false, "success"])}
+    else {return_vals = return_vals.concat([true, "danger"])};
+    console.log("CALL progress:", [name, date, est], badges, return_vals);
+    return_vals.push(message);
+    return return_vals;
+}
 }
