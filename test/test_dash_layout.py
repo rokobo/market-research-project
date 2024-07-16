01168179c8bdcbdd7b93d605fb2665dddd3cddf2
Kobos <61996066+rokobo@users.noreply.github.com>
2024-07-16 10:24:25 -0300

    better performance

--- test/test_dash_layout.py
+++ test/test_dash_layout.py
@@ -26,13 +26,18 @@ AUTHENTICATED_URL = BASE_URL.replace(
 import time
 
 
-def test_initial_first_section(dash_br, dash_process_server):
+def set_server(dash_br, dash_process_server):
     dash_process_server(**PROCESS_ARGS)
     dash_br.driver.get(AUTHENTICATED_URL)
     dash_br.driver.get(BASE_URL)
+    dash_br.driver.set_window_position(10**4, 0)
     time.sleep(1)
     dash_br.wait_for_page(BASE_URL)
     dash_br.driver.get(BASE_URL)
+
+
+def test_initial_first_section(dash_br, dash_process_server):
+    set_server(dash_br, dash_process_server)
 
     for id in FIRST_IDS:
         dash_br.wait_for_element_by_id(id)
@@ -44,12 +49,7 @@ def test_initial_first_section(dash_br, dash_process_server):
 
 
 def test_interaction_first_section(dash_br, dash_process_server):
-    dash_process_server(**PROCESS_ARGS)
-    dash_br.driver.get(AUTHENTICATED_URL)
-    dash_br.driver.get(BASE_URL)
-    time.sleep(1)
-    dash_br.wait_for_page(BASE_URL)
-    dash_br.driver.get(BASE_URL)
+    set_server(dash_br, dash_process_server)
 
     for id in FIRST_IDS:
         dash_br.wait_for_element_by_id(id)
@@ -70,18 +70,14 @@ def test_interaction_first_section(dash_br, dash_process_server):
 
 
 def test_initial_second_section(dash_br, dash_process_server):
-    dash_process_server(**PROCESS_ARGS)
-    dash_br.driver.get(AUTHENTICATED_URL)
-    dash_br.driver.get(BASE_URL)
-    time.sleep(1)
-    dash_br.wait_for_page(BASE_URL)
-    dash_br.driver.get(BASE_URL)
+    set_server(dash_br, dash_process_server)
 
     products = [PRODUCTS[0], PRODUCTS[-1]]
     for product in products:
+        section_div = dash_br.driver.find_element_by_id(f"{product}-heading")
         dash_br.wait_for_element(f'[id*="price-{product}"]')
-        productElements = [dash_br.find_elements(f'[id*="{field}-{product}"]')
-            for field in FIELDS]
+        productElements = [section_div.find_elements_by_css_selector(
+            f'[id*="{field}-{product}"]') for field in FIELDS]
 
         assert len(productElements[1]) == PRODUCT_ROWS[product], product
         for idx, elements in enumerate(productElements):
@@ -92,12 +88,7 @@ def test_initial_second_section(dash_br, dash_process_server):
 
 
 def test_interaction_second_section(dash_br, dash_process_server):
-    dash_process_server(**PROCESS_ARGS)
-    dash_br.driver.get(AUTHENTICATED_URL)
-    dash_br.driver.get(BASE_URL)
-    time.sleep(1)
-    dash_br.wait_for_page(BASE_URL)
-    dash_br.driver.get(BASE_URL)
+    set_server(dash_br, dash_process_server)
 
     for product in PRODUCTS:
         dash_br.wait_for_element(f'[id*="price-{product}"]')
@@ -115,12 +106,7 @@ def test_interaction_second_section(dash_br, dash_process_server):
 
 
 def test_deletion_second_section(dash_br, dash_process_server):
-    dash_process_server(**PROCESS_ARGS)
-    dash_br.driver.get(AUTHENTICATED_URL)
-    dash_br.driver.get(BASE_URL)
-    time.sleep(1)
-    dash_br.wait_for_page(BASE_URL)
-    dash_br.driver.get(BASE_URL)
+    set_server(dash_br, dash_process_server)
 
     for product in PRODUCTS:
         dash_br.wait_for_element(f'[id*="delete-{product}"]')
@@ -137,37 +123,30 @@ def test_deletion_second_section(dash_br, dash_process_server):
 
 
 def test_correct_second_section(dash_br, dash_process_server):
-    dash_process_server(**PROCESS_ARGS)
-    dash_br.driver.get(AUTHENTICATED_URL)
-    dash_br.driver.get(BASE_URL)
-    time.sleep(1)
-    dash_br.wait_for_page(BASE_URL)
-    dash_br.driver.get(BASE_URL)
+    set_server(dash_br, dash_process_server)
 
-    for product in PRODUCTS:
+    for idx, product in enumerate(PRODUCTS):
+        section_div = dash_br.driver.find_element_by_id(f"{product}-heading")
         dash_br.wait_for_element(f'[id*="price-{product}"]')
-        brands = dash_br.find_elements(f'[id*="brand-{product}"]')
-        prices = dash_br.find_elements(f'[id*="price-{product}"]')
-        quantities = dash_br.find_elements(f'[id*="quantity-{product}"]')
-        observations = dash_br.find_elements(f'[id*="obs-{product}"]')
-        dash_br.wait_for_element_by_id(f"status-{product}")
-        badge = dash_br.driver.find_element_by_id(f"status-{product}")
+        badge = section_div.find_element_by_id(f"status-{product}")
         dash_br.driver.execute_script("arguments[0].scrollIntoView(true);",
             badge)
         time.sleep(0.15)
-
         assert "danger" in badge.get_attribute("class")
-        for brand, price, quantity, obs in zip_longest(
-            brands, prices, quantities, observations, fillvalue=None
-        ):
-            if brand is not None:
-                brand.send_keys(
-                    brand.find_elements_by_tag_name('option')[-1].text)
-            assert "danger" in badge.get_attribute("class")
-            if price is not None:
-                price.send_keys(20)
-            if quantity is not None:
-                quantity.send_keys(1.9)
-            if obs is not None:
+        selector = [
+            f'[id*="brand-{product}"]', f'[id*="price-{product}"]',
+            f'[id*="quantity-{product}"]', f'[id*="obs-{product}"]']
+
+        if idx < 9:
+            for brd in section_div.find_elements_by_css_selector(selector[0]):
+                brd.send_keys(brd.find_elements_by_tag_name('option')[-1].text)
+        for prc in section_div.find_elements_by_css_selector(selector[1]):
+            prc.send_keys(20)
+        if idx < 8:
+            for qty in section_div.find_elements_by_css_selector(selector[2]):
+                qty.send_keys(1.9)
+        if idx > 8:
+            for obs in section_div.find_elements_by_css_selector(selector[3]):
                 obs.send_keys("observation")
+
         assert "success" in badge.get_attribute("class")
