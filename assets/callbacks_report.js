1f65757849cb04304b9d34c370625e1e53ce2981
Kobos <61996066+rokobo@users.noreply.github.com>
2024-08-19 19:53:41 -0300

    row style change

--- assets/callbacks_report.js
+++ assets/callbacks_report.js
@@ -2,20 +2,20 @@ if(!window.dash_clientside){window.dash_clientside={}}
 window.dash_clientside.report={
 refresh_files:async function(_,disabled,fileHash,files){
     if(disabled){return [NOUPDATE,fileHash,files]}
-    if(!Array.isArray(fileHash)||fileHash.length!=2){fileHash=[0,0]}
+    if(!Array.isArray(fileHash)||fileHash.length!=2||files.length==0){fileHash=[0,0]}
     const response=await fetch('/get-file-names',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({hash:fileHash[0]||''})});
     const data=await response.json();
     if(data.updated&&files.length==fileHash[1]){INFO("File hash is the same");return [files,fileHash,files]}
     const agData=data.file_names.map(fileName=>{
         const parts=fileName.replace(".csv", "").split('|');
         fileRow={Nome:parts[2],Data:`${parts[0]}, ${new Date(parts[1]*1000).toLocaleString("en-CA",{hour12:false})}`};
-        if(!parts[4]||!parts[5]){fileRow.Loc="Sem coordenadas";fileRow.Estab=parts[3]}
+        if(!parts[4]||!parts[5]){fileRow.Loc="Sem coordenadas";fileRow.Estab=parts[3].split(" ")[0]}
         else {
             estabNear=nearest(parts[4],parts[5]);
             estabNear=`${estabNear[1].split(" ")[0]}, ${Math.round(estabNear[0]*1000)}`;
             distEstab=Math.round(1000*haversine([parts[4],parts[5]],[COORDS[parts[3]].Latitude,COORDS[parts[3]].Longitude]));
             fileRow.Loc=`${parts[4]}, ${parts[5]}, (${estabNear}m)`;
-            fileRow.Estab=`${parts[3]}, ${distEstab}m`;
+            fileRow.Estab=`${parts[3].split(" ")[0]}, ${distEstab}m`;
         }
         return fileRow
     });fileHash=[data.hash,agData.length];INFO("File hash updated",agData.length,data,agData);return [agData,fileHash,agData]
