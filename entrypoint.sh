890b73f0b28235268c853f16eac66630003f0fcc
Kobos <61996066+rokobo@users.noreply.github.com>
2025-04-24 00:57:19 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 cd /ICB
 
-pip install --no-cache-dir -r requirements.txt
+python3.12 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+
+gunicorn src.main:server --daemon -b 127.0.0.1:8060
