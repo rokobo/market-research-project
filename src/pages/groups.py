39b103bf3660e27debea5ec744e20b385633b03c
Kobos <61996066+rokobo@users.noreply.github.com>
2025-04-24 01:06:38 -0300

    Update groups.py

--- src/pages/groups.py
+++ src/pages/groups.py
@@ -5,8 +5,10 @@ from CONFIG import CFG
 
 dash.register_page(__name__, path_template="/categoria/<group>")
 
+
 def layout(group=None):
     if group in set(CFG.groups):
+        assert type(group) is str, "Grupo deve ser uma string"
         return create_page(group)
     else:
         return html.H1("Grupo não encontrado", style={"color": "red"})
